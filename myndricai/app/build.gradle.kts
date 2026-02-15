1598e07d5af0501dd157832d30933261d44ef2aa
Marshinina Alisa <amarshinina@korusconsulting.ru>
2026-02-15 23:29:07 +0300

    Промежуточный этап

--- myndricai/app/build.gradle.kts
+++ myndricai/app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
 
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.gridlayout:gridlayout:1.0.0")
 
 
     // Дополнительные библиотеки
